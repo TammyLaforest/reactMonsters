dcbfd8a8dbecb1f75463609154e0f4b0d8b0d0ab
TammyLaforest <TammyLaforest@gmail.com>
2019-11-14 11:50:04 -0500

    Updates

---
+++ precache-manifest.8adb354782c0a7842613269342e70244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ab60100ae99256b4f6b996e81236cd",
+    "url": "/reactMonsters/index.html"
+  },
+  {
+    "revision": "63ce4d2fc181295ab6af",
+    "url": "/reactMonsters/static/css/main.ac5c4255.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/reactMonsters/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "63ce4d2fc181295ab6af",
+    "url": "/reactMonsters/static/js/main.247562e8.chunk.js"
+  },
+  {
+    "revision": "5ed314cb9e2068aeb07f",
+    "url": "/reactMonsters/static/js/runtime-main.951c4d15.js"
+  }
+]);
